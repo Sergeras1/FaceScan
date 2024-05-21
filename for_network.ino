a255b2f7a456213c94c53e5bcc55fc60aebaaeca
Coffee <142932792+Sergeras1@users.noreply.github.com>
2024-05-21 16:35:58 +0500

    Добавлен код с дисплеем

--- for_network.ino
+++ for_network.ino
@@ -1,6 +1,10 @@
 #include <Servo.h>
+#include <Wire.h>                       
+#include <LiquidCrystal_I2C.h> 
 
 Servo servo;
+LiquidCrystal_I2C LCD(0x27,16,2); 
+
 double angle = 0; // начальное значение угла
 int ledPin = 13; // Пин светодиода
 
@@ -8,12 +12,17 @@ void setup()
 {
     pinMode(ledPin, OUTPUT);
     Serial.begin(9600);
+    LCD.init();            
+    LCD.backlight(); 
     servo.attach(3); // указываем номер пина, к которому подключен сервопривод
     servo.write(0); // устанавливаем начальное положение сервопривода
 }
 
 void loop()
 {
+  LCD.setCursor(0, 0);
+  LCD.print("Face ");
+   
     if (Serial.available())
     {
         char val = Serial.read();
@@ -22,16 +31,22 @@ void loop()
         {
            angle = 0; // если значение больше 0.5, устанавливаем угол 0 градусов
             digitalWrite(ledPin, LOW); // Выключить светодиод
-           
+            LCD.setCursor(0, 0);
+            LCD.print("Face no");
+            delay(3000);
+            LCD.clear();
+            delay(200);
         }
         else if (val == '1')
         {
          servo.write(90); // если значение меньше 0.5, устанавливаем угол 90 градусов
             digitalWrite(ledPin, HIGH); // Включить светодиод
-             delay(2000);
-             servo.write(0);
+            LCD.setCursor(0, 0);
+            LCD.print("Face ok");
+            delay(5000);
+            servo.write(0);
+            LCD.clear();
+            delay(200);
         }
     }
-    //servo.write(angle); // отправляем угол на сервопривод
-  //delay(20); // задержка для стабильной работы
 }
